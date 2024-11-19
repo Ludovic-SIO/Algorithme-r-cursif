a78bf48dad4c0d16e5a4a2ae254e007496457eda
lchamprobert <lchamprobert@N112-04>
2024-11-19 08:51:51 +0100

    Ajoutez des fichiers projet.

---
+++ Algorithme récursif/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithme_récursif
+{
+    internal class Program
+    {
+        static int recursif(int bn)
+        {
+            if (bn == 0) 
+            { 
+                return 1; 
+            } 
+            else 
+            { 
+                return bn * recursif(bn - 1); 
+            }
+        }
+           
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Saisie un entier");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("La factorielle de n est {0} ",recursif(a));
+            Console.ReadKey();
+        }
+    }
+}
